1b1628662f60cd9e69425c5db9f16859b4f764b6
GulliFerWD <112898483+DEVudg22@users.noreply.github.com>
2026-05-05 09:31:24 -0600

    RESPUESTA SERVER MODIFICADA
    Se añadio a la respuesta de autenticacion el nombre y rol

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,6 +33,10 @@ class UserController extends Controller
         return response()->json([
             'status' => true,
             'message' => 'Usuario autenticado correctamente',
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'second_surname' => $user->second_surname,
+            'role' => $user->role,
             'token' => $user->createToken("API TOKEN")->plainTextToken
         ], 200);
     }
